0ede0bb5fe54ba775de3f091dc3180e6002cfc0f
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-12 19:38:01 -0700

    refactor: isolated catalog item into a separate component.

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,12 +1,10 @@
-import { useCallback, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 import api from '../services/api';
-import { addProductToCart } from '../store/modules/cart/actions';
 import { IProduct } from '../store/modules/cart/types';
+import { CatalogItem } from './CatalogItem';
 
 export default function Catalog() {
-    const dispatch = useDispatch();
     const [catalog, setCatalog] = useState<IProduct[]>([]);
 
     useEffect(() => {
@@ -15,20 +13,11 @@ export default function Catalog() {
         });
     }, []);
 
-    const handleAddProductToCart = useCallback((product: IProduct) => {
-        dispatch(addProductToCart(product));
-    }, [dispatch]);
-
     return (
         <main>
             <h1>Catalog</h1>
             {catalog.map(product => (
-                <article key={product.id}>
-                    <strong>{product.title}</strong> {" - "}
-                    <span>{product.price}</span> {"  "}
-
-                    <button type="button" onClick={() => handleAddProductToCart(product)}>Add to cart</button>
-                </article>
+                <CatalogItem key={product.id} product={product} />
             ))}
         </main>
     );
